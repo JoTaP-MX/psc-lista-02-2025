5b524b561de80811c3a89e48a97599e837952420
JoTaP-MX <jpcordeiro.gc@gmail.com>
2025-03-13 20:43:21 -0300

    Update Exercício

--- ViagemEurotrip.java
+++ ViagemEurotrip.java
@@ -4,15 +4,15 @@ public class ViagemEurotrip{
      
      Scanner scanner = new Scanner(System.in);
 
-      System.out.println("X: ");
+      System.out.println("Digite o preço da viagem para Alemanha: ");
         double passagemAlemanha = scanner.nextDouble();
-      System.out.println("X: ");
+      System.out.println("Digite o preço da viagem para Italia: ");
         double passagemItalia = scanner.nextDouble();
-      System.out.println("X: ");
+      System.out.println("Digite o preço da viagem para Portugal: ");
         double passagemPortugal = scanner.nextDouble();
-      System.out.println("X: ");
+      System.out.println("Escreva o número de pessoas que participarão para a viagem: ");
         double passageiros = scanner.nextDouble();
-        double pagar = (passageiros);
+        double valortotal = (passageiros * ());
     
       System.out.println("\n O valor que deve ser pago pelo cliente é : " + pagar+"R$");
 
